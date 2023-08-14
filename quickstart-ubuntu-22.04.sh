16ae4cee36da880b4fa1dd2526cff7064d90d673
Haroldo Cruz <haroldocruzdr@gmail.com>
2023-08-14 11:38:00 -0300

    update: quickstart :: vscode

--- quickstart-ubuntu-22.04.sh
+++ quickstart-ubuntu-22.04.sh
@@ -9,10 +9,19 @@ sudo apt install git
 
 
 echo "...INSTALANDO VSCODE..."
-sudo add-apt-repository "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main"
-wget -q https://packages.microsoft.com/keys/microsoft.asc -O- | sudo apt-key add -
+# sudo add-apt-repository "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main"
+# wget -q https://packages.microsoft.com/keys/microsoft.asc -O- | sudo apt-key add -
+# sudo apt update
+# sudo apt install code
+sudo apt-get install wget gpg
+wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
+sudo install -D -o root -g root -m 644 packages.microsoft.gpg /etc/apt/keyrings/packages.microsoft.gpg
+sudo sh -c 'echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/keyrings/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" > /etc/apt/sources.list.d/vscode.list'
+rm -f packages.microsoft.gpg
+
+sudo apt install apt-transport-https
 sudo apt update
-sudo apt install code
+sudo apt install code # or code-insiders
 
 
 echo "...INSTALANDO DOCKER..."
